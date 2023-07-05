5a7d830f40b95b11b8e589164311bb139f97859b
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-07-05 15:03:54 +0530

    Login SignUp added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const multer = require("multer");
 const app = express();
 const port = 3000;
@@ -10,7 +11,38 @@ app.get("/", (req, res) => {
 	res.render("index", { name: "", login: false });
 });
 
-app.get("/login", (req, res) => {});
+//Route Login
+app
+	.route("/login")
+	.get((req, res) => {
+		res.render("login");
+	})
+	.post((req, res) => {
+		res.render("login");
+	});
+
+//Route SignUp
+app
+	.route("/signup")
+	.get((req, res) => {
+		res.render("signup", { error: "" });
+	})
+	.post((req, res) => {
+		fs.readFile("dbb.txt", "utf-8", (err, data) => {
+			if (err) {
+				res.render("signup", { error: "Users not Found" });
+				return;
+			}
+
+			let users = [];
+			if (data.length > 0 && data[0] === "{" && data[data.length - 1] === "}") {
+			}
+		});
+	});
+
+app.get("*", (req, res) => {
+	res.send(404);
+});
 
 app.listen(port, () => {
 	console.log("Listening at port : " + port);
